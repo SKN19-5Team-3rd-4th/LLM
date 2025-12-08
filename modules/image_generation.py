0096c5fe86aee265196f097d2ddb27aa6d17e5b7
deneb784 <deneb784@gmail.com>
2025-12-08 23:55:48 +0900

    refactor: #46/issue image generation 파이프라인 초안 구성

--- modules/image_generation.py
+++ modules/image_generation.py
@@ -2,35 +2,103 @@ from openai import OpenAI
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 
 import json
-from typing import TypedDict, Annotated, Optional, Literal, List
+from typing import TypedDict, Annotated, Optional, Literal, List, Dict
 from PIL import Image
 import io
 import base64
 from dotenv import load_dotenv
 
-def image_to_base64(path):
+
+def image_to_base64(path) -> base64 :
     with open(path, "rb") as f:
         return base64.b64encode(f.read()).decode('utf-8')
-    
-def base64_to_image(b64_image):
+
+# base64를 출력용 이미지로 변환 
+def base64_to_image(b64_image) -> Image.Image :
     image_data = base64.b64decode(b64_image)
     return Image.open(io.BytesIO(image_data))
+
+# Image형태를 모델에 전달하기 위한 형태의 bytes 형태로 변환
+def image_to_bytes(image: Image.Image, path: str = None, format="PNG") -> bytes :
+    # 경로가 존재한다면 이미지를 저장 후 bytes 형태로 오픈
+    if path:
+        image.save(path, format=format)
+        return open(path, "rb")
+    # 경로가 존재하지 않는다면 버퍼에 저장 후 오픈
+    else:
+        buf = io.BytesIO()
+        image.save(buf, format=format)
+        buf.seek(0)
+        return buf
+    
+def bytes_to_base64(img):
+    return base64.b64encode(img.read()).decode('utf-8')
 
 class ImageGenerator() :
     def __init__(self):
         load_dotenv()
         self.client = OpenAI()
 
-    def image_generation_with_response(self, room_img: base64, flower_img: base64, prompt=None) :
+    def generate_image_summarize(self, input_image: bytes, system_prompt:str, user_prompt:str, reference_text: str = None) -> Dict :
+        image_base64 = bytes_to_base64(input_image)
+
+        response = self.client.chat.completions.create(
+            model = 'gpt-5.1',
+            input=[
+                {
+                    "role": "system",
+                    "content" :[
+                        {
+                            "type" : "input_text",
+                            "text" : system_prompt
+                        }
+                    ]
+                },
+                {
+                    "role": "user",
+                    "content" :[
+                        {
+                            "type" : "input_text", 
+                            "text" : user_prompt,
+                        },
+                        {
+                            "type" : "input_text", 
+                            "text" : f"참고 예시 : {reference_text}",
+                        },
+
+                        {
+                            "type" : "input_image",
+                            "image_url" : f"data:image/png;base64,{image_base64}",
+                        }
+                    ]
+                }
+            ]
+        )
+
+        return response.choices[0].message.content
+
+    def generate_image_with_response(self, room_img_bytes: bytes, flower_img_bytes: bytes, system_prompt, user_prompt=None) :
+
+        room_img = bytes_to_base64(room_img_bytes)
+        flower_img = bytes_to_base64(flower_img_bytes)
         response = self.client.responses.create(
             model="gpt-5.1",
             input=[
+                {
+                    "role": "system",
+                    "content" :[
+                        {
+                            "type" : "input_text",
+                            "text" : system_prompt
+                        }
+                    ]
+                },
                 {
                     "role": "user",
                     "content" : [
                         {
                             "type" : "input_text", 
-                            "text" : prompt,
+                            "text" : user_prompt,
                         },
                         {
                             "type" : "input_image",
@@ -58,10 +126,10 @@ class ImageGenerator() :
         else: 
             return None
         
-    def image_generation_with_image_edit(self, room_img_path: str, flower_img_path: str, prompt=None) :
+    def generate_image_with_image_edit(self, room_img: bytes, flower_img: bytes, prompt=None) :
         response = self.client.images.edit(
             model="gpt-image-1",
-            image=[open(room_img_path, 'rb'), open(flower_img_path, 'rb')],
+            image=[room_img, flower_img],
             prompt=prompt,
             input_fidelity="high",
         )
