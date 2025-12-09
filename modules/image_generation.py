697e6beaa3625ca40426a09e35cc519be5478bc5
deneb784 <deneb784@gmail.com>
2025-12-09 11:30:21 +0900

    refactor: #46/issue 이미지 생성 모델 입력 형태 통일, 파이프라인 정리

--- modules/image_generation.py
+++ modules/image_generation.py
@@ -19,43 +19,6 @@ def base64_to_image(b64_image) -> Image.Image :
     image_data = base64.b64decode(b64_image)
     return Image.open(io.BytesIO(image_data))
 
-# Image형태를 모델에 전달하기 위한 형태의 bytes 형태로 변환
-def image_to_bytes(image: Image.Image, path: str = None, format="PNG", is_bytes=True) -> bytes :
-    # 경로가 존재한다면 이미지를 저장 후 bytes 형태로 오픈
-    if path:
-        image.save(path, format=format)
-        with open(path, "rb") as f:
-            if is_bytes is True:
-                data = f.read()
-                return io.BytesIO(data)
-            else:
-                return f
-
-    # 경로가 존재하지 않는다면 버퍼에 저장 후 오픈
-    tmp = tempfile.NamedTemporaryFile(delete=False, suffix=f".{format.lower()}")
-    image.save(tmp.name, format=format)
-    tmp.close()  # 반드시 닫아야 아래 open() 가능
-    with open(tmp.name, "rb") as f:
-        if is_bytes is True:
-            data = f.read()
-            return io.BytesIO(data)
-        else:
-            return f
-    
-def bytes_to_base64(img):
-    # BytesIO 인 경우 커서를 처음으로 이동
-    if isinstance(img, io.BytesIO):
-        img.seek(0)
-        data = img.read()
-    else:
-        # bytes라면 그대로 사용
-        data = img
-
-    return base64.b64encode(data).decode('utf-8')
-
-def bytesio_to_buffered_reader(bio: io.BytesIO) -> BufferedReader:
-    bio.seek(0)  # 반드시 처음으로 이동
-    return BufferedReader(bio)
 
 
 class ImageGenerator() :
@@ -63,8 +26,8 @@ class ImageGenerator() :
         load_dotenv()
         self.client = OpenAI()
 
-    def generate_image_summarize(self, input_image: io.BytesIO, system_prompt:str, user_prompt:str, reference_text: str = None) -> Dict :
-        image_base64 = bytes_to_base64(input_image)
+    def generate_image_summarize(self, input_image_path: str, system_prompt:str, user_prompt:str, reference_text: str = None) -> Dict :
+        image_base64 = image_open_to_base64(input_image_path)
 
         response = self.client.chat.completions.create(
             model = 'gpt-4o',
@@ -105,10 +68,10 @@ class ImageGenerator() :
 
         return response.choices[0].message.content
 
-    def generate_image_with_response(self, room_img_bytes: BytesIO, flower_img_bytes: BytesIO, system_prompt, user_prompt=None) -> Image.Image :
+    def generate_image_with_response(self, room_img_path: str, flower_img_path: str, system_prompt, user_prompt=None) -> Image.Image :
 
-        room_img = bytes_to_base64(room_img_bytes)
-        flower_img = bytes_to_base64(flower_img_bytes)
+        room_img = image_open_to_base64(room_img_path)
+        flower_img = image_open_to_base64(flower_img_path)
         response = self.client.responses.create(
             model="gpt-5.1",
             input=[
